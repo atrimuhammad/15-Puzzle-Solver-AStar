6a648a142d44175ae8c6ff8a1d1ac5c168fe900f
Muhammad Athar <53816070+atrimuhammad@users.noreply.github.com>
2020-12-31 06:24:49 +0500

    Update Tree.h

--- Tree.h
+++ Tree.h
@@ -56,7 +56,7 @@ class Tree
 		T_Node* getRootNode();
 
 		int* getChildrenArray(T_Node* node);
-///
+
 		void BlankTileRowandColumn(T_Node* node, int& blank_row, int& blank_col);
 		void PossibleChildren(T_Node* node, int blank_row, int blank_col);
 		void MakeTempPuzzle(int**& temp, T_Node* node);
@@ -65,14 +65,14 @@ class Tree
 		void MakeDownChild(T_Node* node, T_Node* parent_node, int blank_row, int blank_col);
 		void MakeLeftChild(T_Node* node, T_Node* parent_node, int blank_row, int blank_col);
 		void MakeRightChild(T_Node* node, T_Node* parent_node, int blank_row, int blank_col);
-///
+
 		void MakeChildren(T_Node* node, T_Node* parent_node, Tree T, int NoT);
-///
+
 		void UpChildHeuristicValue(T_Node* node, int G, int** goal);
 		void DownChildHeuristicValue(T_Node* node, int G, int** goal);
 		void LeftChildHeuristicValue(T_Node* node, int G, int** goal);
 		void RightChildHeuristicValue(T_Node* node, int G, int** goal);
-///
+	
 		void FindHeuristicValues(T_Node* node, int G, Tree T, int NoT);
 
 		T_Node* FindPath(T_Node* node, int* children);
